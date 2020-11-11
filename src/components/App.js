8d2501d17fac69ef8242c7091c259191c1218627
Dhirendra-byte <68893214+Dhirendra-byte@users.noreply.github.com>
2020-11-12 00:22:44 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,10 @@ import '../styles/App.css';
 const App = () => {
   return (
     <>
+  <div value="data-ns-test">
     <h1 value="project-name" >Medical Related Website</h1>
     <p value="project-description" >project descryption is here......</p>
+    </div>
     </>
   )
 }
